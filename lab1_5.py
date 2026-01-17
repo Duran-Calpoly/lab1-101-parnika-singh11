4f4a20a466177ab8cabbfb54251ad50f4bba098d
parnika-singh11 <parnika110307@gmail.com>
2026-01-17 02:40:31 +0000

    finished lab 5

--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,22 @@
-pass
+def check_multiple(number):
+    if number%3 == 0 and number%5==0:
+        return True
+    else:
+        return False
+    
+def check_password(input_string):
+    password = "Python123"
+    if input_string == password:
+        return "access granted"
+    else:
+        return "access denied"
+    
+def calculate_federal_tax(salary):
+    if salary<=11000:
+        return(10/100*salary)
+    elif salary<=44725:
+        return(12/100*salary)
+    elif salary<=95375:
+        return(22/100*salary)
+    else:
+        return(24/100*salary)
